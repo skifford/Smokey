48f828fd2ac53dd99aac84610c87e42c222065ef
Eugene Kotovrasov <eugene.kotovrasov@bimeister.com>
2022-07-24 10:14:10 +0300

    chore: removed old files

--- demo/Smokey.Demo.MSTest.Ctors/Extensions/StringExtensions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Smokey.Demo.MSTest.Ctors.Extensions
-{
-    public static class StringExtensions
-    {
-        public static bool HasValue(this string str) => string.IsNullOrWhiteSpace(str) is false;
-    }
-}
